e3e8bab30704f0e395b4dfddd043c6cbd53230b5
David <davidqm7@outlook.com>
2024-09-26 17:55:40 -0400

    firebase

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 import {getAuth} from "firebase/auth"; 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCNVqABto6QjXAdh57yxKgA7YSYchEAA3s",  // process.env.REACT_APP_FIREBASE_API_KEY
-  authDomain: "chat-application-b414e.firebaseapp.com",  //process.env.REACT_APP_FIREBASE_AUTH_DOMAIN
-  projectId: "chat-application-b414e",   //process.env.REACT_APP_FIREBASE_PROJECT_ID
-  storageBucket: "chat-application-b414e.appspot.com",   //process.env.REACT_APP_FIREBASE_STORAGE_BUCKET
-  messagingSenderId:"794236905156",   //process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
-  appId: "1:794236905156:web:51694920f4c9a99ebf0b91"   //process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey:  process.env.REACT_APP_FIREBASE_API_KEY,  // process.env.REACT_APP_FIREBASE_API_KEY
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,  //process.env.REACT_APP_FIREBASE_AUTH_DOMAIN
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,   //process.env.REACT_APP_FIREBASE_PROJECT_ID
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,   //process.env.REACT_APP_FIREBASE_STORAGE_BUCKET
+  messagingSenderId:process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,   //process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
+  appId: process.env.REACT_APP_FIREBASE_APP_ID   //process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
